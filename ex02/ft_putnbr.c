f17a8c4090f8c3cf7cebe07c43d68902031c77b2
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-08-04 04:44:14 +0930

    Complete ex02

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+void	rec(int nb)
+{
+	char	ch;
+
+	if (nb == 0)
+		return ;
+	if (nb / 10)
+		rec(nb / 10);
+	ch = '0' + nb % 10;
+	write(1, &ch, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	char	ch;
+
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		rec(-(nb / 10));
+		ch = '0' - nb % 10;
+	}
+	else
+	{
+		rec(nb / 10);
+		ch = '0' + nb % 10;
+	}
+	write(1, &ch, 1);
+}
